89ed0b68874ccb82d1a3d708ffeba587fdfc677b
Joseph Young <musicjoeyoung@gmail.com>
2024-09-10 14:10:14 -0400

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import warehouseRoutes from "./routes/warehouseRoute.js";
 import inventoryRoutes from "./routes/inventoryRoute.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
